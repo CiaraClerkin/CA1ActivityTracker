6ace0bb38a9c4e75470696a779fe2ba435ae4729
CiaraClerkin <145358793+CiaraClerkin@users.noreply.github.com>
2023-10-25 10:51:20 +0100

    Added comparators for the Strings

---
+++ src/ActivityComparator.java
@@ -0,0 +1,8 @@
+import java.util.Comparator;
+
+public class ActivityComparator implements Comparator<ActivityTracked> {
+    @Override
+    public int compare(ActivityTracked at1, ActivityTracked at2) {
+        return at1.getActivity().compareTo(at1.getActivity());
+    }
+}
