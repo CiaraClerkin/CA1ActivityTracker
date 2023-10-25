b5fe4cd15d1f11d3449b6300f634050f9534417b
CiaraClerkin <145358793+CiaraClerkin@users.noreply.github.com>
2023-10-25 09:49:05 +0100

    Added ActivityTracked class.

---
+++ src/ActivityTracked.java
@@ -0,0 +1,70 @@
+public class ActivityTracked {
+    private String activity;
+    private double duration;
+    private String date;
+    private double distance;
+    private double avgHeartRate;
+
+    public ActivityTracked(String activity, double duration, String date, double distance, double avgHeartRate) {
+        this.activity = activity;
+        this.duration = duration;
+        this.date = date;
+        this.distance = distance;
+        this.avgHeartRate = avgHeartRate;
+    }
+
+    public ActivityTracked() {
+
+    }
+
+    public String getActivity() {
+        return activity;
+    }
+
+    public void setActivity(String activity) {
+        this.activity = activity;
+    }
+
+    public double getDuration() {
+        return duration;
+    }
+
+    public void setDuration(double duration) {
+        this.duration = duration;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
+
+    public double getAvgHeartRate() {
+        return avgHeartRate;
+    }
+
+    public void setAvgHeartRate(double avgHeartRate) {
+        this.avgHeartRate = avgHeartRate;
+    }
+
+    @Override
+    public String toString() {
+        return "ActivityTracked{" +
+                "activity='" + activity + '\'' +
+                ", duration=" + duration +
+                ", date='" + date + '\'' +
+                ", distance=" + distance +
+                ", avgHeartRate=" + avgHeartRate +
+                '}';
+    }
+}
